d6dadbbe26bb087b09006f1966958842b903119a
Aidan <support@fyreempire.net>
2023-11-21 21:55:30 -0800

    image scaling

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,20 +1,20 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import 'app/globals.css'
-import Image from 'next/image'
-import Link from 'next/link'
 
 export default function index() {
   return (
-    <main className="flex flex-col justify-between p-20">
-      <div className="flex justify-between items-center">
+    <main className="flex flex-col justify-between h-screen">
+      <header className="header">
         <h1 className="pb-6 pt-4 text-2xl text-left lg:w-auto lg:p-4">
-          About
+          Apex Legends Renders
         </h1>
         <nav className="pb-6 pt-4 text-xl text-right lg:w-auto lg:p-4">
-        <Link href="/"><a className="p-2">Home</a></Link>
-        <Link href="/about"><a className="p-2">About</a></Link>
-        <Link href="/requests"><a className="p-2">Requests</a></Link>
+          <Link href="/" className="p-2">Home</Link>
+          <Link href="/about" className="p-2">About</Link>
+          <Link href="/requests" className="p-2">Requests</Link>
         </nav>
-      </div>
+      </header>
     </main>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,21 +5,23 @@ import 'app/globals.css'
 export default function index() {
   return (
     <main className="flex flex-col justify-between h-screen">
-      <header className="header flex justify-between items-center p-8">
+      <header className="header">
         <h1 className="pb-6 pt-4 text-2xl text-left lg:w-auto lg:p-4">
           Apex Legends Renders
         </h1>
         <nav className="pb-6 pt-4 text-xl text-right lg:w-auto lg:p-4">
-        <Link href="/"><a className="p-2">Home</a></Link>
-        <Link href="/about"><a className="p-2">About</a></Link>
-        <Link href="/requests"><a className="p-2">Requests</a></Link>
+          <Link href="/" className="p-2">Home</Link>
+          <Link href="/about" className="p-2">About</Link>
+          <Link href="/requests" className="p-2">Requests</Link>
         </nav>
       </header>
-      <div className="flex items-center ml-20">
+      <div className="flex items-center justify-center">
         <div className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl'> {/* Add Tailwind classes for responsive text size */}
           <strong>Welcome.</strong>
         </div>
-          <img src="/images/Gilded_Radiance1.png"/>
+        <div className="ml-auto">
+          <Image src="/images/Gilded_Radiance1.png" alt="Conduit" width={1000} height={2160}/>
+        </div>
       </div>
     </main>
   )
